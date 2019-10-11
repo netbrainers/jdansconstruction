37ee90a2b79017f321a5212eaa269a159eba1974
VICTOR <netbrainers@gmail.com>
2019-10-11 16:13:10 +0100

    load speed optimization

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,3 +24,54 @@ function toggleService(event) {
     btnOpen.classList.toggle('toggle-hide');
     btnClose.classList.toggle('toggle-show');
 }
+
+// == animation JS Style ==
+
+//demo 02
+$("#demo01").animatedModal({
+    animatedIn:'lightSpeedIn',
+    animatedOut:'bounceOutDown',
+    color:'#fff',
+    // Callbacks
+    beforeOpen: function() {
+        console.log("The animation was called");
+    },           
+    afterOpen: function() {
+        console.log("The animation is completed");
+    }, 
+    beforeClose: function() {
+        console.log("The animation was called");
+    }, 
+    afterClose: function() {
+        console.log("The animation is completed");
+    }
+});
+$("#demo02").animatedModal({
+    animatedIn:'zoomIn',
+    animatedOut:'bounceOut',
+    color:'#fff',
+    // Callbacks
+    beforeOpen: function() {
+        console.log("The animation was called");
+    },           
+    afterOpen: function() {
+        console.log("The animation is completed");
+    }, 
+    beforeClose: function() {
+        console.log("The animation was called");
+    }, 
+    afterClose: function() {
+        console.log("The animation is completed");
+    }
+});
+
+// == Netlify Form JS Style ==
+
+$("#contact_form").submit(function(e) {
+    e.preventDefault();
+  
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+      alert("Thank you! You'll get a feedback from us shortly.");
+    });
+  });
